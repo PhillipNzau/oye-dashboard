32a26a24aebef226ec6661dcdad39f3191f39c6a
phillipnzau <nzauonline@gmail.com>
2023-09-01 02:06:43 +0300

    home with table implementation

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -9,5 +9,30 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  tableData: any[] = [
+    {
+      title: '1',
+      mobile: '123-456-7890',
+      amount: '$100',
+      airTimeForPayment: '10 days',
+      status: 'Pending',
+      airTimeStatus: 'Active',
+      date: '2023-09-15',
+      time: '14:30',
+      receiptNumber: '12345'
+    },
+    {
+      title: '2',
+      mobile: '987-654-3210',
+      amount: '$200',
+      airTimeForPayment: '5 days',
+      status: 'Paid',
+      airTimeStatus: 'Inactive',
+      date: '2023-09-17',
+      time: '10:00',
+      receiptNumber: '54321'
+    },
+    // Add more mock data items here
+  ];
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'myTeal': '#81AEB8',
+      },
+    },
   },
   plugins: [],
 }
