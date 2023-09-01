715b76a1941e4bef7213371c17bd8bf49b906064
phillipnzau <nzauonline@gmail.com>
2023-09-01 03:28:46 +0300

    router link

--- src/app/dashboard/shared/components/navbar/navbar.component.ts
+++ src/app/dashboard/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor(
+    private router: Router
+  ) {}
+
+  isRouteActive(routePath:string):boolean {
+    return this.router.url == routePath
+  }
 
 }
