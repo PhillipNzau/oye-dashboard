6f04b5b9cfa3f207c944613bd75346d16efe8c4d
phillipnzau <nzauonline@gmail.com>
2023-09-05 01:00:40 +0300

    customers page setup

---
+++ src/app/dashboard/shared/components/customers-table/customers-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-customers-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './customers-table.component.html',
+  styleUrls: ['./customers-table.component.scss']
+})
+export class CustomersTableComponent {
+
+}
