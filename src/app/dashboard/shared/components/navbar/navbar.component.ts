182e05698a58b68bef7deac110803f928686210c
phillipnzau <nzauonline@gmail.com>
2023-09-05 14:14:02 +0300

    navbar complete

--- src/app/dashboard/shared/components/navbar/navbar.component.ts
+++ src/app/dashboard/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 
@@ -9,11 +9,33 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent  implements OnInit{
+  @Output() triggerMenu: EventEmitter<boolean> = new EventEmitter<boolean>();
+  isSmallScreen: boolean = false;
   constructor(
     private router: Router
   ) {}
   activeRoute:string ='';
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {    
+    this.isSmallScreen = window.innerWidth <= 768; // Adjust breakpoint as needed
+  }
+
+  ngOnInit(): void {
+    this.checkScreenSize()
+  }
+
+  trigger() {
+    if (this.isSmallScreen) {
+      this.triggerMenu.emit(false)
+    }
+  }
 
   isRouteActive(routePath:string):boolean {
     this.activeRoute = this.router.url
